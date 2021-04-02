756a90819fec7a73df34df2213ae08e4d7b6cff8
Josiah Leon <sjosiahleon@gmail.com>
2021-04-02 14:37:44 -0700

    Created knightpathfinder.rb

---
+++ W3D5/knights_travails/knigntpathfinder.rb
@@ -0,0 +1,14 @@
+class KnightPathFinder
+
+    def initialize(position)
+        @position = position
+        @x = position[0]
+        @y = position[1]
+        @considered_positions = [@position]
+        # @root_node = 
+    end
+
+    def self.vaild_moves(position)
+
+    end
+end
