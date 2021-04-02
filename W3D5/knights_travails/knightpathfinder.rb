e1ea0cebf9ae5f2c3750d73637681ffed26dd99c
Josiah Leon <sjosiahleon@gmail.com>
2021-04-02 15:13:57 -0700

    Added Phase-2

--- W3D5/knights_travails/knightpathfinder.rb
+++ W3D5/knights_travails/knightpathfinder.rb
@@ -9,18 +9,31 @@ class KnightPathFinder
     end
 
     def self.valid_moves(position)
-        moves = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1]. [-2, 1], [-2, -1]]
+        moves = [[1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1]]
         answer = []
         moves.each do |move|
             x, y = position
             new_position = [(x + move[0]), (y + move[1])]
-            next if new_position.all? { |num| num <= 0 && num >= 7 }
+            next if new_position.any? { |num| num <= 0 || num >= 7 }
             answer << new_position
         end
         answer
     end
 
     def new_move_positions(position)
+        moves = KnightPathFinder.valid_moves(position)
+        new_moves = moves.select {|el| !@considered_positions.include?(el) }
+        @considered_positions.concat(new_moves) unless new_moves.empty?
+        new_moves
+    end
+
+    def build_move_tree
+        start_pos = @position
+        queue = [start]
+        
     end
 
 end
+
+# k = KnightPathFinder.new([0,0])
+# p k.new_move_positions([0,0])
