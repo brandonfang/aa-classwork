9330dfaaffefe24c4a65a0cf0869171311595428
brandonfang <bdmfang@gmail.com>
2021-04-05 14:55:15 -0700

    Started phase 2

--- W4D1/tic_tac_toe_ai/lib/super_computer_player.rb
+++ W4D1/tic_tac_toe_ai/lib/super_computer_player.rb
@@ -2,6 +2,13 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    other_mark = (mark == :x) ? :o : :x
+    node = TicTacToeNode.new(game, mark)
+    node.children.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+      return child.prev_move_pos unless child.losing_node?(mark)
+    end
+    raise "No non-losing nodes. Game is tied."
   end
 end
 
