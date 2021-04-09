683104074f1fab7d6777c502fad8ac0db5d6bb4a
Juan Barba <barbajuan11@gmail.com>
2021-04-09 13:50:37 -0700

    Phase 2 contiguous subsum

--- w4d5/execution.rb
+++ w4d5/execution.rb
@@ -15,6 +15,7 @@ def my_min_2(arr)
 end
 
 def largest_contiguous_subsum_1(arr)
+  return 0 if arr.empty?
   subarrays = contiguous_subarrays(arr)
   max = subarrays[0].sum
   subarrays.each do |subarr|
@@ -39,6 +40,40 @@ end
 # list2 = [-5, -1, -3]
 # p largest_contiguous_subsum_1(list2) # => -1 (from [-1])
 
+def largest_contiguous_subsum_2(arr)
+  return 0 if arr.empty?
+  max_sum = 0
+  cur_sum = 0
+  i = 0
+
+  (0...arr.length).each do |j|
+
+    cur_sum += arr[j] #-14
+
+    max_sum = cur_sum if cur_sum > max_sum
+
+    while cur_sum >= max_sum
+
+      cur_sum -= arr[i]
+      i+=1
+    end
+
+  end
+
+  max_sum
+end
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum_2(list) # => 8 (from [7, -6, 7])
+
+list2 = [-5, -1, -3]
+p largest_contiguous_subsum_2(list2) # => -1 (from [-1])
+
+list3 = [-5, 6, 2, -1, -8,  -3]
+p largest_contiguous_subsum_2(list3) # => 8 (from [])
+
+list4 = [4, -20, -5, 6, 2, -1]
+p largest_contiguous_subsum_2(list4) # => 8 (from [])
 
 
 
