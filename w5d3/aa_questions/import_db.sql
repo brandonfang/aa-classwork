1d9ad9223ee5490428f68e6f2142f0310de62e14
brandonfang <bdmfang@gmail.com>
2021-04-14 14:11:36 -0700

    Create tables

--- w5d3/aa_questions/import_db.sql
+++ w5d3/aa_questions/import_db.sql
@@ -1,2 +1,52 @@
--- Tables: users, questions, question_follows, replies, question_likes
 PRAGMA foreign_keys = ON;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL,
+)
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (author_id) REFERENCES users(id)
+)
+
+CREATE TABLE question_follows (
+    question_id INTEGER PRIMARY KEY,
+    follower_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (follower_id) REFERENCES users(id)
+)
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    subject_question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
+    author_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+
+    FOREIGN KEY (subject_question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
+    FOREIGN KEY (author_id) REFERENCES users(id),
+)
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+)
+
+INSERT INTO
+  users (fname, lname)
+VALUES
+  ('Steven', 'Tran'),
+  ('Brandon', 'Fang');
