b0d33e07bdf150266c9cf311c42ab96fd3d595d9
StevenTran91 <sdtran91@gmail.com>
2021-04-14 14:50:02 -0700

    completed import_db.sql file

--- w5d3/aa_questions/import_db.sql
+++ w5d3/aa_questions/import_db.sql
@@ -1,10 +1,13 @@
-PRAGMA foreign_keys = ON;
+-- PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS users 
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
-)
+    lname TEXT NOT NULL
+);
+
+DROP TABLE IF EXISTS questions
 
 CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
@@ -13,7 +16,10 @@ CREATE TABLE questions (
     author_id INTEGER NOT NULL,
 
     FOREIGN KEY (author_id) REFERENCES users(id)
-)
+);
+
+
+DROP TABLE IF EXISTS question_follows
 
 CREATE TABLE question_follows (
     question_id INTEGER PRIMARY KEY,
@@ -22,7 +28,10 @@ CREATE TABLE question_follows (
 
     FOREIGN KEY (question_id) REFERENCES questions(id),
     FOREIGN KEY (follower_id) REFERENCES users(id)
-)
+);
+
+
+DROP TABLE IF EXISTS replies
 
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
@@ -34,19 +43,47 @@ CREATE TABLE replies (
     FOREIGN KEY (subject_question_id) REFERENCES questions(id),
     FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
     FOREIGN KEY (author_id) REFERENCES users(id),
-)
+);
 
+
+
+    
+DROP TABLE IF EXISTS question_likes
 CREATE TABLE question_likes (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
-    question_id INTEGER NOT NULL
+    question_id INTEGER NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES users(id)
     FOREIGN KEY (question_id) REFERENCES questions(id)
-)
+);
+
 
 INSERT INTO
   users (fname, lname)
 VALUES
   ('Steven', 'Tran'),
   ('Brandon', 'Fang');
+
+INSERT INTO 
+    questions (title, body, author_id)
+VALUES 
+    ("LRU Cache", "How do you implement an LRU Cashe?", 1),
+    ("Flexbox", "What is justify-content?", 2);
+
+INSERT INTO 
+    question_follows (question_id, follower_id, author_id)
+VALUES
+    (1, 2, 1),
+    (1, 1, 1),
+    (2, 2, 2);
+
+INSERT INTO 
+    replies(subject_question_id, author_id, body)
+VALUES
+    (2, 1, "Justify-Content defines how space is distributed along the main axis of a flex container");
+
+INSERT INTO 
+    question_likes(user_id, question_id)
+VALUES
+    (1, 2);
