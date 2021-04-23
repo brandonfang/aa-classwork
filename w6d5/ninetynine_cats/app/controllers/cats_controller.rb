cb858bf07ee8c8cdc2f7459e0fbb1da4fbade5e7
brandonfang <bdmfang@gmail.com>
2021-04-23 16:18:13 -0700

    Update index and show pages

--- w6d5/ninetynine_cats/app/controllers/cats_controller.rb
+++ w6d5/ninetynine_cats/app/controllers/cats_controller.rb
@@ -4,12 +4,16 @@ class CatsController < ApplicationController
         @cats = Cat.all 
 
         render :index
-
     end
 
     def show
-
-
+        @cat = Cat.find_by(id: params[:id])
+        render :show
     end
 
+    private 
+
+    def cat_params
+        params.require(:cat).permit(:name, :color, :birth_date, :sex, :description)
+    end
 end
