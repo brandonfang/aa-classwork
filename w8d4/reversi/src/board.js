5d19b8ffafc218738a52839b11d2e38a3326a663
brandonfang <bdmfang@gmail.com>
2021-05-06 11:45:52 -0700

    Start reversi

--- w8d4/reversi/src/board.js
+++ w8d4/reversi/src/board.js
@@ -9,6 +9,17 @@ if (typeof window === 'undefined'){
  * and two white pieces at [3, 3] and [4, 4]
  */
 function _makeGrid () {
+  let grid = new Array(8);
+  for (let i = 0; i < grid.length; i++) {
+    grid[i] = new Array(8);
+  }
+
+  grid[3][4] = new Piece("black");
+  grid[4][3] = new Piece("black");
+  grid[3][3] = new Piece("white");
+  grid[4][4] = new Piece("white");
+
+  return grid;
 }
 
 /**
@@ -16,6 +27,7 @@ function _makeGrid () {
  */
 function Board () {
   this.grid = _makeGrid();
+
 }
 
 Board.DIRS = [
--- w8d4/reversi/src/piece.js
+++ w8d4/reversi/src/piece.js
@@ -2,25 +2,29 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+  this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+  return (this.color === "white") ? "black" : "white";
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+  this.color = this.oppColor();
 };
 
 /**
- * Returns a string representation of the string
+ * Returns a string representation of the piece
  * based on its color.
  */
 Piece.prototype.toString = function () {
+  return (this.color === "white") ? "W" : "B";
 };
 
 // DON'T TOUCH THIS CODE
