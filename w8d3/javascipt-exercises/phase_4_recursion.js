8751d231375d2d5bda010bca574a093b69617d03
brandonfang <bdmfang@gmail.com>
2021-05-05 17:26:09 -0700

    Update phase 4

--- w8d3/javascipt-exercises/phase_4_recursion.js
+++ w8d3/javascipt-exercises/phase_4_recursion.js
@@ -2,26 +2,20 @@ function range(start, end) {
     if (start > end) { return false };
     if (start === end) { return [end] };
     return [start].concat(range(start + 1, end));
-
     // return (range(start, end - 1).push(end));
 }
 
 
 function sumRec(arr) {
     if (arr.length === 0) {return 0};
-    
     return arr[0] + sumRec(arr.slice(1));
 }
-
-// console.log(sumRec([]));
 
 function exponent(base, exp) {
     if (exp === 0) {return 1};
 
     return base * exponent(base, exp - 1);
 }
-
-// console.log(exponent(2,0));
 
 function exponent_2(base, exp) {
     if (exp === 0) {return 1};
@@ -34,8 +28,6 @@ function exponent_2(base, exp) {
     };
 }
 
-// console.log(exponent_2(2,0));
-
 function fibonacci(n) {
     if (n < 1) {return false}
     if (n === 1) {return [0]}
@@ -45,8 +37,6 @@ function fibonacci(n) {
 
     return temp.concat([temp[temp.length-2] + temp[temp.length-1]])
 }
-
-// console.log(fibonacci(10));
 
 function deepDup(arr) {
     if (!Array.isArray(arr)) {return arr};
@@ -63,7 +53,17 @@ function deepDup(arr) {
     return result;
 }
 
-let arr = [1,2,[3,[4,5]]];
-let arr_1 = deepDup(arr);
-arr.push(7)
-console.log(arr)
+function bsearch(arr, target) {
+    if (arr.length < 2 && arr[0] !== target) { return -1 };
+    let mid = Math.floor(arr.length / 2);
+    if (arr[mid] === target) return mid;
+    if (target < arr[mid]) {
+        return bsearch(arr.slice(0, mid), target);
+    } else {
+        if (bsearch(arr.slice(mid + 1), target) !== -1) {
+            return bsearch(arr.slice(mid + 1), target) + mid + 1;
+        } else {
+            return -1;
+        }
+    }
+}
