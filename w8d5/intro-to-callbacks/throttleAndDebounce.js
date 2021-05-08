6f8a14830d7e787d3ac263212db4ce2e8b58666a
brandonfang <bdmfang@gmail.com>
2021-05-07 17:09:01 -0700

    Attempt debounce

--- w8d5/intro-to-callbacks/throttleAndDebounce.js
+++ w8d5/intro-to-callbacks/throttleAndDebounce.js
@@ -1,52 +1,49 @@
 Function.prototype.myThrottle = function(time) {
-    let tooSoon = false;
+  let tooSoon = false;
 
-    return(...arguments) => {
-        if (tooSoon === false) {
-            tooSoon = true
-            setTimeout( () => {tooSoon = false}, time);
-            this(...arguments);
-        }
+  return(...arguments) => {
+    if (tooSoon === false) {
+      tooSoon = true
+      setTimeout(() => { tooSoon = false }, time);
+      this(...arguments);
     }
+  }
 };
     
+// class Neuron {
+//   fire() {
+//     console.log("Firing!");
+//   }
+// }
 
+// const neuron = new Neuron();
+// // When we create a new Neuron,
+// // we can call #fire as frequently as we want
 
-
-class Neuron {
-  fire() {
-    console.log("Firing!");
-  }
-}
-
-const neuron = new Neuron();
-// When we create a new Neuron,
-// we can call #fire as frequently as we want
-
-// The following code will try to #fire the neuron every 10ms. Try it in the console:
+// // The following code will try to #fire the neuron every 10ms. Try it in the console:
 // const interval = setInterval(() => {
 //   neuron.fire();
 // }, 10);
 
-// You can use clearInterval to stop the firing:
+// // You can use clearInterval to stop the firing:
 // clearInterval(interval);
 
-// Using Function#myThrottle, we should be able to throttle
-// the #fire function of our neuron so that it can only fire
-// once every 500ms:
+// // Using Function#myThrottle, we should be able to throttle
+// // the #fire function of our neuron so that it can only fire
+// // once every 500ms:
 
-neuron.fire = neuron.fire.myThrottle(500);
+// neuron.fire = neuron.fire.myThrottle(500);
 
-const interval = setInterval(() => {
-  neuron.fire();
-}, 10);
+// const interval = setInterval(() => {
+//   neuron.fire();
+// }, 10);
 
-// This time, if our Function#myThrottle worked correctly,
-// the Neuron#fire function should only be able to execute
-// every 500ms, even though we're still trying to invoke it
-// every 10ms!
+// // This time, if our Function#myThrottle worked correctly,
+// // the Neuron#fire function should only be able to execute
+// // every 500ms, even though we're still trying to invoke it
+// // every 10ms!
 
-// If we want this behavior for ALL neurons, we can do the same logic in the constructor:
+// // If we want this behavior for ALL neurons, we can do the same logic in the constructor:
 
 // class Neuron {
 //   constructor() {
@@ -57,3 +54,64 @@ const interval = setInterval(() => {
 //     console.log("Firing!");
 //   }
 // }
+
+
+
+class SearchBar {
+  constructor() {
+    this.query = "";
+
+    this.type = this.type.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  type(letter) {
+    this.query += letter;
+    this.search();
+  }
+
+  search() {
+    console.log(`searching for ${this.query}`);
+  }
+}
+
+const searchBar = new SearchBar();
+
+const queryForHelloWorld = () => {
+  searchBar.type("h");
+  searchBar.type("e");
+  searchBar.type("l");
+  searchBar.type("l");
+  searchBar.type("o");
+  searchBar.type(" ");
+  searchBar.type("w");
+  searchBar.type("o");
+  searchBar.type("r");
+  searchBar.type("l");
+  searchBar.type("d");
+};
+
+queryForHelloWorld();
+
+
+Function.prototype.myDebounce = (interval) => {
+
+  let countdown = setTimeout(() => {
+    // check if debounced function has been invoked before countdown
+  }, interval);
+
+
+
+  return (...arguments) => {
+
+    // setTimeOut()
+
+    // this(...arguments)
+
+  };
+};
+
+
+searchBar.search = searchBar.search.myDebounce(500);
+
+queryForHelloWorld();
