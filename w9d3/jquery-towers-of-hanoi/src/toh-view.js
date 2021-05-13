b6230a3417fa84ff040eb5a25cfb0677a7677c70
Brenda J <linjiangda@yahoo.com>
2021-05-12 20:08:55 -0400

    Worked on hanoi

--- w9d3/jquery-towers-of-hanoi/src/toh-view.js
+++ w9d3/jquery-towers-of-hanoi/src/toh-view.js
@@ -17,15 +17,25 @@ class View {
   setupTowers() {
     let $board = $('<div>').addClass('board');
     for (let i = 0; i < 3; i++) {
-      let pile = $('<ul>').addClass('pile');
-      $board.append(pile);
+      let $pile = $('<ul>').addClass('pile');
+      for (let i = 0; i < 3; i++) {
+        let $disc = $('<li>')
+        $pile.append($disc);
+      }
+      $board.append($pile);
     }
+
+    // let $pile1 = $('.board:first-child');
+    $('ul li:nth-child(1)').addClass('disc-one')
+
     // console.log($('ul.pile').eq(0));
-    let $pile1 = $('ul.pile').eq(0);
-    $pile1.append($('<li>')).addClass('disc-one');
-    $pile1.append($('<li>')).addClass('disc-two');
-    $pile1.append($('<li>')).addClass('disc-three');
-    
+    // let $pile1 = $('.pile').eq(0);
+    // console.log($pile1)
+    // for (let i = 0; i < 3; i++) {
+    //   let disc = $('<li>').addClass('disc');
+    //   $pile1.append(disc);
+    // }
+
     this.$el.append($board);
   }
 
