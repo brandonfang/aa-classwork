336a094bf3ce4e1de3f373af2f568e43f4b65997
brandonfang <bdmfang@gmail.com>
2021-05-20 13:47:56 -0700

    Add

---
+++ w10d4/frontend/todo-redux.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  const content = <h1>Todo App</h1>
+  ReactDOM.render(content, root);
+});
