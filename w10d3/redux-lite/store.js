531158f0ba8f23ca3fb15051d8ed81affea411a7
brandonfang <bdmfang@gmail.com>
2021-05-20 08:31:50 -0700

    Add redux lite

---
+++ w10d3/redux-lite/store.js
@@ -0,0 +1,13 @@
+class Store {
+  constructor(rootReducer) {
+    this.rootReducer = rootReducer;
+    this.state = {
+      
+    };
+    this.getState = this.getState.bind(this);
+  }
+
+  getState() {
+    return Object.assign({}, this.state);
+  }
+}
